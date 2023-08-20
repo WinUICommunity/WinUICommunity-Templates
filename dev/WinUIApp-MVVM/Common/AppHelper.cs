ad4efdac234bb3e7091b88a90e4a3cfe27ddc7e5
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-08-20 19:11:42 +0330

    Move Files From VSIX To ProjectTemplates

---
+++ dev/WinUIApp-MVVM/Common/AppHelper.cs
@@ -0,0 +1,17 @@
+﻿using Nucs.JsonSettings;
+using Nucs.JsonSettings.Autosave;
+using Nucs.JsonSettings.Fluent;
+using Nucs.JsonSettings.Modulation;
+using Nucs.JsonSettings.Modulation.Recovery;
+
+namespace $safeprojectname$.Common;
+
+public static partial class AppHelper
+{
+    public static AppConfig Settings = JsonSettings.Configure<AppConfig>()
+                               .WithRecovery(RecoveryAction.RenameAndLoadDefault)
+                               .WithVersioning(VersioningResultAction.RenameAndLoadDefault)
+                               .LoadNow()
+                               .EnableAutosave();
+}
+
