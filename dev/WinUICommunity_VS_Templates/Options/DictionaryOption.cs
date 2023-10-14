1b83f8339a395a4d6f141d65a310149bdd416dbf
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-10-14 13:09:48 +0330

    Update DictionaryOption.cs

--- dev/WinUICommunity_VS_Templates/Options/DictionaryOption.cs
+++ dev/WinUICommunity_VS_Templates/Options/DictionaryOption.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using HandyControl.Tools.Extension;
+using System.Collections.Generic;
 
 namespace WinUICommunity_VS_Templates.Options
 {
@@ -10,48 +11,48 @@ namespace WinUICommunity_VS_Templates.Options
             {
                 if (useColorsDic)
                 {
-                    replacementsDictionary.Add("$AppDicColors$", "<ResourceDictionary Source=\"Themes/Colors.xaml\" />");
+                    replacementsDictionary.AddIfNotExists("$AppDicColors$", "<ResourceDictionary Source=\"Themes/Colors.xaml\" />");
                 }
                 else
                 {
-                    replacementsDictionary.Add("$AppDicColors$", "");
+                    replacementsDictionary.AddIfNotExists("$AppDicColors$", "");
                 }
             }
             
             if (useHomeLandingPage && hasNavigationView)
             {
-                replacementsDictionary.Add("$AppDicColors$", "<ResourceDictionary Source=\"Themes/Colors.xaml\" />");
+                replacementsDictionary.AddIfNotExists("$AppDicColors$", "<ResourceDictionary Source=\"Themes/Colors.xaml\" />");
             }
             else
             {
-                replacementsDictionary.Add("$AppDicColors$", "");
+                replacementsDictionary.AddIfNotExists("$AppDicColors$", "");
             }
 
             if (useFontsDic)
             {
-                replacementsDictionary.Add("$AppDicFonts$", "<ResourceDictionary Source=\"Themes/Fonts.xaml\" />");
+                replacementsDictionary.AddIfNotExists("$AppDicFonts$", "<ResourceDictionary Source=\"Themes/Fonts.xaml\" />");
             }
             else
             {
-                replacementsDictionary.Add("$AppDicFonts$", "");
+                replacementsDictionary.AddIfNotExists("$AppDicFonts$", "");
             }
 
             if (useConvertersDic)
             {
-                replacementsDictionary.Add("$AppDicConverters$", "<ResourceDictionary Source=\"Themes/Converters.xaml\" />");
+                replacementsDictionary.AddIfNotExists("$AppDicConverters$", "<ResourceDictionary Source=\"Themes/Converters.xaml\" />");
             }
             else
             {
-                replacementsDictionary.Add("$AppDicConverters$", "");
+                replacementsDictionary.AddIfNotExists("$AppDicConverters$", "");
             }
 
             if (useStylesDic)
             {
-                replacementsDictionary.Add("$AppDicStyles$", "<ResourceDictionary Source=\"Themes/Styles.xaml\" />");
+                replacementsDictionary.AddIfNotExists("$AppDicStyles$", "<ResourceDictionary Source=\"Themes/Styles.xaml\" />");
             }
             else
             {
-                replacementsDictionary.Add("$AppDicStyles$", "");
+                replacementsDictionary.AddIfNotExists("$AppDicStyles$", "");
             }
         }
     }
