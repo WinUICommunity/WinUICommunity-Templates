eb4ec3ef035703202af25dad211855cde970657c
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-10-14 14:42:22 +0330

    Fix Some Issue

--- dev/WinUICommunity_VS_Templates/Shell/SettingsCardOptions.cs
+++ dev/WinUICommunity_VS_Templates/Shell/SettingsCardOptions.cs
@@ -142,8 +142,11 @@
         private async void NavigateToLogPath_Click(object sender, RoutedEventArgs e)
         {
             string folderPath = (sender as HyperlinkButton).Content.ToString();
-            Windows.Storage.StorageFolder folder = await Windows.Storage.StorageFolder.GetFolderFromPathAsync(folderPath);
-            await Windows.System.Launcher.LaunchFolderAsync(folder);
+            if (Directory.Exists(folderPath))
+            {
+                Windows.Storage.StorageFolder folder = await Windows.Storage.StorageFolder.GetFolderFromPathAsync(folderPath);
+                await Windows.System.Launcher.LaunchFolderAsync(folder);
+            }
         }
 """";
     }
