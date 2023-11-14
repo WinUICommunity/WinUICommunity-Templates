5de43cd3a9d95ee4744f036a1a5c23fd5ae90b7d
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-11-14 23:10:41 +0330

    Fix Missing Tag

--- dev/WinUIApp/Common/AppConfig.cs
+++ dev/WinUIApp/Common/AppConfig.cs
@@ -8,7 +8,7 @@ public class AppConfig : JsonSettings, IVersionable
     [EnforcedVersion("1.0.0.0")]
     public virtual Version Version { get; set; } = new Version(1, 0, 0, 0);
 
-    public override string FileName { get; set; } = Constants.AppConfigPath;
+    public override string FileName { get; set; } = Constants.AppConfigPath;$DeveloperModeConfig$
 
     // Docs: https://github.com/Nucs/JsonSettings
 }
