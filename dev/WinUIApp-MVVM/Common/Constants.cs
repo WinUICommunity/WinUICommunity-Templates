e69c4b4789baf4477dd18b59f734f096838722e8
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-08-03 18:12:29 +0330

    Refactor

--- dev/WinUIApp-MVVM/Common/Constants.cs
+++ dev/WinUIApp-MVVM/Common/Constants.cs
@@ -3,5 +3,5 @@
 public static class Constants
 {
     public static readonly string AppName = ProcessInfoHelper.GetProductNameAndVersion();
-    public static readonly string RootDirectoryPath = Path.Combine(PathHelper.GetApplicationDataFolderPath(), AppName);$SerilogDirectoryPath$$SerilogFilePath$$AppConfigFilePath$
+    public static readonly string RootDirectoryPath = Path.Combine(PathHelper.GetAppDataFolderPath(), AppName);$SerilogDirectoryPath$$SerilogFilePath$$AppConfigFilePath$
 }
